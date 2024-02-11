839270540397650c308074e0a435a561b55598df
maycohennn <103824299+maycohennn@users.noreply.github.com>
2024-02-11 14:59:59 +0200

    Add files via upload

---
+++ jScripts/javascript.js
@@ -0,0 +1,249 @@
+const jsonData = {
+    "concepts": [
+        {
+            "id": 1,
+            "title": "דמוקרטיה",
+            "image":"images/democracy-pic.png",
+            "content": "שיטת משטר שבה רצונם של האזרחים בא לידי ביטוי בדרך של הצבעה.",
+            "moreContent":"לאזרחי מדינה דמוקרטית זכות חוקית להשפיע על המדיניות הציבורית. לדוגמה, באמצעות הבעת דעות בפומבי, הצבעה בבחירות או במשאלי עם, השתתפות בדיונים לפני ההחלטות ובהחלטות עצמן, התמודדות בבחירות או הצטרפות אל מפלגה המתמודדת בהן. דמוקרטיות בנות ימינו מעניקות משקל משמעותי לעקרונות שמבססים את יכולת האזרח לממש זכות זו, ובהם שמירת זכויות אדם כמו הזכות לחירות, חופש הביטוי, חופש התאגדות, זכות הקניין וחופש התנועה.",
+            "tag": "NotI"
+        },
+        {
+            "id": 2,
+            "title": "חוקה",
+            "image":"images/constitution-pic.png",
+            "content": "מכלול של עקרונות יסוד או תקדימים מבוססים המהווים בסיס משפטי ומנהלי של המשטר במדינה, ארגון או סוג אחר של ישות.",
+            "moreContent":"חוקה נוצרת על ידי גוף מכונן, וחייבת בהגדרתה לכלול את הוראות היסוד בדבר מבנה המשטר והשלטון, סמכויות החקיקה, צורת בחירת השלטון וכיוצא באלו. רוב החוקות עוסקות גם בנושא זכויות האדם הבסיסיות של תושבי המדינה, וזכויות של קבוצות מיעוט. בפדרציה ובקונפדרציה החוקה גם מסדירה את סמכויות השלטון המרכזי וסמכויות השלטון המקומי-מדינתי.",
+            "tag": "NotI"
+        },
+        {
+            "id": 3,
+            "title": "חוקי יסוד",
+            "image":"images/laws-pic.png",
+            "content": "הדרך שבחרה הכנסת לשם גיבוש הדרגתי של חוקה לישראל, לאור המחלוקת ששררה בין חברי הכנסת הראשונה בנוגע לעצם יצירתה של חוקה.",
+            "moreContent":"מרבית חוקי־היסוד שנחקקו עד כה עוסקים במוסדות המדינה: הכנסת, הממשלה, נשיא המדינה, ועוד. שני חוקי־יסוד נוספים – חוק יסוד: כבוד האדם וחירותו וחוק יסוד: חופש העיסוק – עוסקים בזכויות האדם, וחוק יסוד: ישראל – מדינת הלאום של העם היהודי מעגן את היותה של ישראל מדינה יהודית.",
+            "tag": "Israel"
+        },
+        {
+            "id": 4,
+            "title": "רפורמה משפטית",
+            "image":"images/legalreform-pic.png",
+            "content": "יוזמת חקיקה שמקדמת ממשלת ישראל השלושים ושבע לביצוע שינויים מהותיים במערכת המשפט בישראל.",
+            "moreContent":"את התוכנית הציג שר המשפטים וסגן ראש הממשלה יריב לוין, בתמיכת ראש הממשלה בנימין נתניהו. בין השינויים בתוכנית המקורית נכללו אישור פסקת התגברות והסדרת פסילת חוקים, ביטול עילת הסבירות, הגדרת תפקיד הייעוץ המשפטי כמייעץ לא מחייב ושינוי הרכב הוועדה לבחירת שופטים. התוכנית הוצגה ב-4 בינואר 2023, שישה ימים לאחר השבעת ממשלת ישראל השלושים ושבע.",
+            "tag": "Israel"
+        },
+        {
+            "id": 5,
+            "title": "שלטון העם",
+            "image":"images/unity-pic.png",
+            "content": "הרעיון העומד בבסיס עקרון זה הינו שהמדינה קמה על-ידי העם ולמען העם.",
+            "moreContent":"עקרון שלטון העם הינו אחד העקרונות המרכזיים וההכרחיים לקיומה של מדינה דמוקרטית. הרעיון העומד בבסיס עקרון זה הינו שהמדינה קמה על-ידי העם ולמען העם. מסיבה זו האזרחים הם מקור הסמכות במדינה, הם הריבון (האדון). אזרחי המדינה מעניקים לשלטון את הסמכות לייצג אותם ולנהל עבורם את המדינה, מתוך מחויבות לעם שאותו הם מייצגים.",
+            "tag": "NotI"
+        },
+        {
+            "id": 6,
+            "title": "חוק השבות",
+            "image":"images/jews-pic.png",
+            "content": "החוק מעניק לכל יהודי שהביע את רצונו להשתקע בישראל את הזכות לעלות לישראל ולקבל תעודת עולה.",
+            "moreContent":"החוק מגדיר מי זכאי לעלות לישראל ולקבל תעודת עולה. בשנת 2014 היו בעולם כ-23 מיליון זכאי חוק השבות, מתוכם כ-9 מיליון שאינם יהודים על פי ההלכה האורתודוקסית.",
+            "tag": "Israel"
+        },
+        {
+            "id": 7,
+            "title": "המנון",
+            "image":"images/hymn-pic.png",
+            "content": "שירים, בדרך כלל מולחנים, המשמשים כסמל של ארגון מסוים, כגון מדינה, ומייצגים את הארגון וערכיו.",
+            "moreContent":"המקור למשמעות הלאומית של ההמנון כהמנון לאומי הוא בתקופת המהפכה הצרפתית, כשהאספה הלאומית הכריזה על המרסייז כהמנון צרפת. כיום נהוג לפתוח או לחתום אירועים לאומיים בהמנון הלאומי. מלבד מדינות, גם ארגונים כגון יחידות צבאיות, ערים או קבוצות ספורט, יוצרים לעצמם המנון.",
+            "tag": "NotI"
+        },
+        {
+            "id": 8,
+            "title": "הזכות לחירות",
+            "image":"images/freedom-pic.png",
+            "content": "האדם הוא יצור עצמאי שמנהל את חייו באוטונומיה.",
+            "moreContent":"לכל אדם יש זכות לקבל החלטות לגבי חייו באופן עצמאי וללא התערבות של גורם חיצוני ולחיות את חייו באופן שהוא רואה לנכון. הזכות לחירות היא זכות טבעית שמוענקת לאדם מעצם היותו אדם; לשלטון אסור לשלול את הזכות הזו כיוון שלא הוא המעניק אותה לאדם.",
+            "tag": "NotI"
+        },
+        {
+            "id": 9,
+            "title": "יונה עם עלה של זית",
+            "image":"images/dove-pic.png",
+            "content": "אחד הסמלים הידועים של מושג השלום.",
+            "moreContent":"מקורו של סמל זה בתיאור המופיע בספר בראשית בו מתוארת היונה שנשלחה על ידי נח בתום המבול למצוא יבשה. היונה שבה אל תיבת נח ובפיה עלה זית שסימל את תום המבול.",
+            "tag": "Israel"
+        },
+        {
+            "id": 10,
+            "title": "הזכות לשיויון",
+            "image":"images/equality-pic.png",
+            "content": "שוויון בזכויות ובחובות בחברה, ללא הבחנה לפי קריטריונים לא רלוונטיים, שעשויים לכלול גזע, מין, גיל, שפה, לאום, דת, נטייה מינית, השקפה פוליטית ואידאולוגיה.",
+            "moreContent":"במדינות רבות בהן קיימת זכות לשוויון ישנן מחלוקות פוליטיות ומשפטיות לגבי רשימת הקריטריונים שיש לכלול בה. כך לדוגמה, רק בחלק מן המדינות הדמוקרטיות קיים איסור אפליה בשל נטייה מינית או בשל היותו של אדם בעל מוגבלות, וגם במדינות שבהן קיים איסור כזה, פרטיו אינם אחידים.",
+            "tag": "NotI"
+        }
+    ]
+}
+
+// on load
+document.addEventListener("DOMContentLoaded", function (event) {
+    jsonLoop();
+    createModal();
+});
+
+// function - toggle the navigation bar
+function toggleNav(){
+    document.getElementById("main-nav").classList.toggle("hide-mobile");
+}
+
+// function - loop through the JSON data and create list items
+function jsonLoop() {
+    const listContainer = document.getElementById("concepts");
+    const list = document.createElement("ul");
+    list.setAttribute("id", "concepts");
+
+    // creating elements for each concept
+    jsonData.concepts.forEach((c) => {
+        const listItem = document.createElement("li");
+        listItem.setAttribute("id", `li_${c.id}`);
+        listItem.setAttribute("data-title", c.title.toLowerCase());
+
+        const conceptPic = document.createElement("img");
+        conceptPic.setAttribute("src", c.image);
+        listItem.appendChild(conceptPic);
+
+        const liTitle = document.createElement("h2");
+        liTitle.appendChild(document.createTextNode(c.title));
+        listItem.appendChild(liTitle);
+
+        const liContent = document.createElement("p");
+        liContent.appendChild(document.createTextNode(c.content));
+        listItem.appendChild(liContent);
+
+        const moreInfoButton = document.createElement("button");
+        moreInfoButton.appendChild(document.createTextNode("למידע נוסף"));
+        moreInfoButton.addEventListener("click", () => clickedTopic(c.id)); // כפתור המיועד לפתיחת פופ אפ עם מידע נוסף
+        listItem.appendChild(moreInfoButton);
+
+        list.appendChild(listItem);
+
+    });
+
+    listContainer.appendChild(list);
+}
+
+// function for searching concepts based on user input
+function searchConcepts() {
+    const searchInput = document.getElementById("searchInput");
+    const searchQuery = searchInput.value.toLowerCase();
+
+    const concepts = document.querySelectorAll("#concepts li");
+
+    concepts.forEach((concept) => {
+        const conceptTitle = concept.getAttribute("data-title");
+
+        if (conceptTitle.includes(searchQuery)) {
+            concept.style.display = "flex";
+        } else {
+            concept.style.display = "none";
+        }
+    });
+}
+
+// function for filtering concepts based on tags
+function filterConcepts(tag) {
+    const searchInput = document.getElementById("searchInput");
+    searchInput.value = ""; // clear search input
+    const concepts = document.querySelectorAll("#concepts li");
+
+    concepts.forEach((concept) => {
+        const conceptId = concept.id.replace("li_", "");
+        const conceptTag = jsonData.concepts.find((c) => c.id.toString() === conceptId).tag;
+
+        if (tag === "All" || conceptTag === tag) {
+            concept.style.display = "flex";
+        } else {
+            concept.style.display = "none";
+        }
+    });
+
+    resetButtonStyles();
+    setActiveButton(tag + "-btn");
+}
+
+// function for resetting button styles
+function resetButtonStyles() {
+    const buttons = document.querySelectorAll(".fillters button");
+    buttons.forEach((button) => {
+        button.classList.remove("active");
+    });
+}
+
+function setActiveButton(buttonId) {
+    console.log("Setting active button:", buttonId);
+
+    const buttons = document.querySelectorAll(".fillters button");
+    buttons.forEach((button) => {
+        button.classList.remove("active");
+    });
+
+    const activeButton = document.getElementById(buttonId);
+    if (activeButton) {
+        activeButton.classList.add("active");
+        console.log("Button set as active:", activeButton);
+    } else {
+        console.log("Button not found with ID:", buttonId);
+    }
+}
+
+// creating Modals for bootstrap
+function createModal() {
+    const modalContainer = document.createElement("div");
+    modalContainer.setAttribute("class", "modal fade");
+    modalContainer.setAttribute("id", "myModal");
+    modalContainer.setAttribute("tabindex", "-1");
+    modalContainer.setAttribute("aria-labelledby", "exampleModalLabel");
+    modalContainer.setAttribute("aria-hidden", "true");
+
+    const modalDialog = document.createElement("div");
+    modalDialog.setAttribute("class", "modal-dialog");
+
+    const modalContent = document.createElement("div");
+    modalContent.setAttribute("class", "modal-content");
+
+    const modalBody = document.createElement("div");
+    modalBody.setAttribute("class", "modal-body");
+    modalBody.setAttribute("id", "modalContent");
+
+    const closeButton = document.createElement("button");
+    closeButton.setAttribute("type", "button");
+    closeButton.setAttribute("class", "btn-close");
+    closeButton.setAttribute("data-bs-dismiss", "modal");
+    closeButton.setAttribute("aria-label", "Close");
+
+    modalContent.appendChild(modalBody);
+    modalContent.appendChild(closeButton);
+    modalDialog.appendChild(modalContent);
+    modalContainer.appendChild(modalDialog);
+
+    document.body.appendChild(modalContainer);
+}
+
+// function to open a modal with information on a clicked topic
+function openModal(title, content, moreContent) {
+    const modalContent = document.getElementById("modalContent");
+    modalContent.innerHTML = `
+        <h2>${title}</h2>
+        <p>${content}</p>
+        <p>${moreContent}</p>
+    `;
+
+    const myModal = new bootstrap.Modal(document.getElementById("myModal"));
+    myModal.show();
+}
+
+// function for opening the more info PopUp
+function clickedTopic(id) {
+    const concept = jsonData.concepts.find(c => c.id === id);
+    if (concept) {
+        openModal(concept.title, concept.content, concept.moreContent);
+    }
+}
